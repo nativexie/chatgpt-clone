e3796036f15c7eb0eb19ae3efc07fecf61551023
Yuwei Xie <pandahack@163.com>
2023-03-24 12:59:48 +0800

    Update app.py
    replace api to ChatCompletion(gpt-3.5-turbo)

--- app.py
+++ app.py
@@ -2,54 +2,72 @@ import os
 import openai
 import gradio as gr
 
-#if you have OpenAI API key as an environment variable, enable the below
+# using local proxy by setting environment variable, if you need a proxy to connect openai api
+# os.environ["http_proxy"] = "http://127.0.0.1:10809"
+# os.environ["https_proxy"] = "http://127.0.0.1:10809"
+# os.environ["no_proxy"] = "localhost,127.0.0.1,::1"
+
+# or using API to set proxy by environment variable(you need to set OPENAI_PROXY before run this app, recommanded)
+# openai.proxy = os.getenv("OPENAI_PROXY") 
+
+#if you have OpenAI API key as an environment variable, enable the below(recommanded)
 #openai.api_key = os.getenv("OPENAI_API_KEY")
 
 #if you have OpenAI API key as a string, enable the below
-openai.api_key = "xxxxxx"
+openai.api_key = "sk-XXX"
+
 
 start_sequence = "\nAI:"
 restart_sequence = "\nHuman: "
 
 prompt = "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: "
 
-def openai_create(prompt):
+def openai_chat_create(msgs):
 
-    response = openai.Completion.create(
-    model="text-davinci-003",
-    prompt=prompt,
-    temperature=0.9,
-    max_tokens=150,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0.6,
-    stop=[" Human:", " AI:"]
-    )
+    response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages= msgs)
 
-    return response.choices[0].text
+    print()
+    print(f'{response["usage"]["prompt_tokens"]} prompt tokens used.')
+    print()
+    return response['choices'][0]['message']['content']
 
-
-
-def chatgpt_clone(input, history):
+def chatgpt_clone(input, history, msg_history):
     history = history or []
-    s = list(sum(history, ()))
-    s.append(input)
-    inp = ' '.join(s)
-    output = openai_create(inp)
+    msg_history = msg_history or [{"role": "system", "content": "You are a helpful assistant."}]
+    msg_history.append({"role": "user", "content": input})
+
+    msgs = msg_history
+    print("msgs: %s" % msgs)
+    print()
+
+    output = openai_chat_create(msgs)
+    print("chatbot reply: %s" % output)
+    print()
+
+    msg_history.append({"role": "assistant", "content": output})
+    print("msg_history: %s" % msg_history)
+    print()
+
     history.append((input, output))
-    return history, history
+    print("history: %s" % history)
+    print()
+
+    return history, history, msg_history, ""
 
 
 block = gr.Blocks()
 
 
 with block:
-    gr.Markdown("""<h1><center>Build Yo'own ChatGPT with OpenAI API & Gradio</center></h1>
+    gr.Markdown("""<h1><center>Build Yo'own ChatGPT with OpenAI API(ChatCompletion) & Gradio</center></h1>
     """)
     chatbot = gr.Chatbot()
     message = gr.Textbox(placeholder=prompt)
     state = gr.State()
+    state_msg = gr.State()
     submit = gr.Button("SEND")
-    submit.click(chatgpt_clone, inputs=[message, state], outputs=[chatbot, state])
+    submit.click(chatgpt_clone, inputs=[message, state, state_msg], outputs=[chatbot, state, state_msg, message])
 
 block.launch(debug = True)
